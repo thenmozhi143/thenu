9b8160bedfe0c483d10859bd3f9b706451004a65
thenmozhi143 <thenmozhi95.kj@gmail.com>
2016-08-20 16:32:30 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+
+public class Yy {
+
+   
+    public static void main(String[] args) {
+ 
+
+int x,fact=1;
+Scanner input=new Scanner(System.in);
+System.out.print("Enter the Integer :");
+x=input.nextInt();
+for(int i=1;i<=x;i++)
+{
+fact=fact*i;
+}
+System.out.print(fact);
+}
+}
+
+    
+    
+
