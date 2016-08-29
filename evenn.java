d1615bd42617039eca660f9da8780e2a004c5335
thenmozhi143 <thenmozhi95.kj@gmail.com>
2016-08-29 19:04:43 +0530

    Add files via upload

---
+++ evenn.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class oddoreven
+{	
+	public static void main(String args[])
+	{
+  	 int x;
+	 System.out.println("enter the integer");
+	 Scanner.in=newScanner(System.in);
+	 x=in.nextInt();
+	 if(x%2==0)
+	 System.out.println("it is even number");
+	 else
+	 System.out.println("it is odd number");
+	}
+}
